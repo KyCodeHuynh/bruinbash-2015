258b9d1ccbe0eb065c49764fab2f6d5946cfa68b
Daniel Huang <danielhuang@g.ucla.edu>
2015-07-12 19:48:33 -0700

    fixed slideshow

--- js/scripts.js
+++ js/scripts.js
@@ -7,29 +7,16 @@ jQuery(document).ready(function(){
     var now = new Date();
     var diff = (date.getTime()/1000) - (now.getTime()/1000);
     var clock = $('.clock').FlipClock(diff,{
-        clockFace: 'DailyCounter',
-        countdown: true}
+    clockFace: 'DailyCounter',
+    countdown: true}
     );
-    var pole = $('#flag-pole');
-    // General time-out function to execute <code> after specified delay
+    var browserHeight=$(window).height()-20;
+    $(".slideshow-img").css({'height':browserHeight + "px"});
+    var pole=$('#flag-pole');
+    var bbash=$('.bbash');
     setTimeout(function(){
-        $('.flag-pole').fadeOut();
-        setTimeout(function() {
-            // Move flag to meet where the bar will be
-            $('.flag-pole').css('height', '667px');
-        }, 100)
         $('.temp-buffer').fadeOut();
-        $('.bbash').fadeIn('slow');
-        setTimeout(function() {
-            $('.flag-pole').fadeIn('slow');
-        }, 200)
-    }, 4000);
-    // var browserHeight=$(window).height();
-    // var pole=$('#flag-pole');
-    // var bbash=$('.bbash');
-    // setTimeout(function(){
-    //     bbash.css({'height':"browserHeight"});
-    //     $('.temp-buffer').fadeOut();
-    //     bbash.fadeIn('slow');
-    // }, 5000);
+        bbash.fadeIn('slow');
+    }, 5000);
+
 });
