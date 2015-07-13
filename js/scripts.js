a399211d006e6d4ab048581c58835da0998c18bb
KyCodeHuynh <KyCodeHuynh@gmail.com>
2015-07-12 19:12:30 -0700

    Add manual delays and repositioning of prototype flag

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,21 @@ jQuery(document).ready(function(){
     var now = new Date();
     var diff = (date.getTime()/1000) - (now.getTime()/1000);
     var clock = $('.clock').FlipClock(diff,{
-    clockFace: 'DailyCounter',
-    countdown: true}
+        clockFace: 'DailyCounter',
+        countdown: true}
     );
-    var pole=$('#flag-pole');
+    var pole = $('#flag-pole');
+    // General time-out function to execute <code> after specified delay
     setTimeout(function(){
+        $('.flag-pole').fadeOut();
+        setTimeout(function() {
+            // Move flag to meet where the bar will be
+            $('.flag-pole').css('height', '667px');
+        }, 100)
         $('.temp-buffer').fadeOut();
         $('.bbash').fadeIn('slow');
-    }, 5000);
+        setTimeout(function() {
+            $('.flag-pole').fadeIn('slow');
+        }, 200)
+    }, 4000);
 });
