964bdbdef39e20652c850760333d1123511466ac
Ames Ma <dewfall@ucla.edu>
2015-09-05 16:20:50 -0700

    added festival

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,28 @@
 
 jQuery(document).ready(function(){
     $('body').height($(document).height()*10);
-
     white('concerts');
     white('festival');
     white('faq');
     white('film');
     white('tickets');
     white('thanks');
+    var logo=$('#logo');
+    logo.css({
+        top:$(window).height()-120+'px',
+        left:$(window).width() *.45+'px'
+    });
+    $('#logo-1').on('click',function(){scrollTo('#home')});
+   $('#concert-button').on('click',function(){scrollTo('#concert-page')});
+
+    $(window).scroll(function(){
+        if($(window).scrollTop()>($(document).height() *.05)){
+            logo.fadeIn('slow');
+        }else{
+            logo.fadeOut('slow');
+        }
+    });
+
 });
 
 function white(button){
@@ -20,36 +35,14 @@ function white(button){
         $(buttonClass).children(".white").removeClass('hidden');
         $(buttonClass).children(buttonID).addClass('hidden');
     });
-
-    $('.clickable').on('click',function(){
-        $('.intro').fadeOut();
-        setTimeout(function(){
-            $.ajax('countdown.html', {
-                success: function (response) {
-                    $('.main').html(response);
-                },
-                error: function(request, errorType, errorMessage) {
-                    alert('Error: ' + errorType + ' with message: ' + errorMessage);
-                },
-                timeout: 4000,
-                beforeSend: function() {
-                    $('.main').addClass('isloading');
-                },
-                complete: function() {
-
-                }
-            });
-        },1000);
-    });
-    x.on('click',function(){
-        responsiveImg();
-        $('.main').fadeOut();
-        $('.intro').fadeIn();
-
-    /*$(buttonClass).find($('.white')).on('mouseout',function(){
+    $(buttonClass).find($('.white')).on('mouseout',function(){
         $(buttonClass).children('.white').addClass('hidden');
         $(buttonClass).children(buttonID).removeClass('hidden');
->>>>>>> 3d02a7142e53714cc9064317b1b165aa8b6b1fc5*/
-
     });
+}
+
+function scrollTo(id){
+    $('html, body').animate({
+        scrollTop: $(id).offset().top
+    }, 2000);
 }
