5c51eec9486e57ddfff5889bbe295b3a7f3781c9
Daniel Huang <danielhuang@g.ucla.edu>
2015-09-05 16:57:03 -0700

    added faq section plus linked everything

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,7 @@ jQuery(document).ready(function(){
     $('#concert-button').on('click',function(){scrollTo('#concert-page')});
     $('#film-button').on('click',function(){scrollTo('#film-page')});
     $('#festival-button').on('click',function(){scrollTo('#festival-page')});
+    $('#faq-button').on('click',function(){scrollTo('#faq-page')});
     $(window).scroll(function(){
         if($(window).scrollTop()>($(document).height() *.05)){
             logo.fadeIn('slow');
@@ -26,6 +27,7 @@ jQuery(document).ready(function(){
             logo.fadeOut('slow');
         }
     });
+    faq();
 });
 
 function white(button){
@@ -45,4 +47,31 @@ function scrollTo(id){
     $('html, body').animate({
         scrollTop:$(id).offset().top
     },2000);
+}
+
+function faq(){
+    $('#faq-ticket').on('click',function(){
+        $('.answers').remove();
+        $.ajax('faq-answers/tickets.html',{
+            success:function(data){$('#faq-answers').html(data).fadeIn()}
+        });
+    });
+    $('#faq-concert').on('click',function(){
+        $('.answers').remove();
+        $.ajax('faq-answers/concert.html',{
+            success:function(data){$('#faq-answers').html(data).fadeIn()}
+        });
+    });
+    $('#faq-festival').on('click',function(){
+        $('.answers').remove();
+        $.ajax('faq-answers/festival.html',{
+            success:function(data){$('#faq-answers').html(data).fadeIn()}
+        });
+    });
+    $('#faq-general').on('click',function(){
+        $('.answers').remove();
+        $.ajax('faq-answers/general.html',{
+            success:function(data){$('#faq-answers').html(data).fadeIn()}
+        });
+    });
 }
