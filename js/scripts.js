3bc929a8e019e0b8f7e77d9a0d6dcbb141b61227
Daniel Huang <danielhuang@g.ucla.edu>
2015-09-04 22:20:13 -0700

    got first animated nav button down

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,17 @@
 
 jQuery(document).ready(function(){
     $('body').height($(document).height());
+
+    $('.concerts').on("mouseenter",function(){
+        $('.white').removeClass('hidden');
+        $('.concerts').addClass('hidden');
+    });
+    white($('.concerts'));
 });
+
+function white(button){
+    $('.white').on('mouseout',function(){
+        $('.white').addClass('hidden');
+        button.removeClass('hidden');
+    });
+}
