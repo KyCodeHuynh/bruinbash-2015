d37eabf7d0c05265d262dfdb0eb181b225334524
Daniel Huang <danielhuang@g.ucla.edu>
2015-09-05 11:26:30 -0700

    added home button, created half of concerts page

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,31 @@
 
 jQuery(document).ready(function(){
     $('body').height($(document).height()*10);
-
     white('concerts');
     white('festival');
     white('faq');
     white('film');
     white('tickets');
     white('thanks');
+    var logo=$('#logo');
+    logo.css({
+        top:$(window).height()-120+'px',
+        left:$(window).width() *.45+'px'
+    });
+    logo.on('click',function(){
+        $('html, body').animate({
+            scrollTop: $("#home").offset().top
+        }, 2000);
+    });
+    $(window).scroll(function(){
+        if($(window).scrollTop()>($(document).height() *.1)){
+            logo.fadeIn('slow');
+        }
+        else{
+            logo.fadeOut();
+        }
+    });
+
 });
 
 function white(button){
