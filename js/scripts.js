807de3bb79830685f8d92111a6d93d573b7b0b91
Daniel Huang <danielhuang@g.ucla.edu>
2015-09-05 21:38:07 -0700

    added sponsors section (kind of)

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@
  */
 
 jQuery(document).ready(function(){
-    $('body').height($(document).height()*10);
+    $('body').height($(document).height()*1.5);
     white('concerts');
     white('festival');
     white('faq');
@@ -16,9 +16,11 @@ jQuery(document).ready(function(){
         left:$(window).width() *.45+'px'
     });
     $('#logo-1').on('click',function(){scrollTo('#home')});
-
-   $('#concert-button').on('click',function(){scrollTo('#concert-page')});
-
+    $('#concert-button').on('click',function(){scrollTo('#concert-page')});
+    $('#film-button').on('click',function(){scrollTo('#film-page')});
+    $('#festival-button').on('click',function(){scrollTo('#festival-page')});
+    $('#faq-button').on('click',function(){scrollTo('#faq-page')});
+    $('#thanks-button').on('click',function(){scrollTo('#thanks-page')});
     $(window).scroll(function(){
         if($(window).scrollTop()>($(document).height() *.05)){
             logo.fadeIn('slow');
@@ -26,7 +28,7 @@ jQuery(document).ready(function(){
             logo.fadeOut('slow');
         }
     });
-
+    faq();
 });
 
 function white(button){
@@ -44,10 +46,33 @@ function white(button){
 
 function scrollTo(id){
     $('html, body').animate({
+        scrollTop:$(id).offset().top
+    },2000);
+}
 
-
-        scrollTop: $(id).offset().top
-    }, 2000);
-
-
+function faq(){
+    $('#faq-ticket').on('click',function(){
+        $('.answers').remove();
+        $.ajax('faq-answers/tickets.html',{
+            success:function(data){$('#faq-answers').html(data).fadeIn()}
+        });
+    });
+    $('#faq-concert').on('click',function(){
+        $('.answers').remove();
+        $.ajax('faq-answers/concert.html',{
+            success:function(data){$('#faq-answers').html(data).fadeIn()}
+        });
+    });
+    $('#faq-festival').on('click',function(){
+        $('.answers').remove();
+        $.ajax('faq-answers/festival.html',{
+            success:function(data){$('#faq-answers').html(data).fadeIn()}
+        });
+    });
+    $('#faq-general').on('click',function(){
+        $('.answers').remove();
+        $.ajax('faq-answers/general.html',{
+            success:function(data){$('#faq-answers').html(data).fadeIn()}
+        });
+    });
 }
